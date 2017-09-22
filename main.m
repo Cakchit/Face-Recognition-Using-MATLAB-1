8bd829246522d46a451e770b9f3e1e04ed66d90b
Arjun Nataraj <aj.nataraj2604@gmail.com>
2017-09-22 22:21:53 +0530

    Fischer's Analysis Completed

--- main.m
+++ main.m
@@ -43,3 +43,24 @@ U = A * pevec;
 
 % Obtaining PCA weights
  Wpca = U'*A;
+
+ % Obtaining Sb and Sw
+ cMean = zeros(M-k,M-k);
+ Sb = zeros(M-k,M-k);
+ Sw = zeros(M-k,M-k);
+
+ pcaMean = mean(Wpca,2);
+
+ for i = 1:k
+     cMean = mean(Wpca(:,n*i-(n-1):n*i),2);
+     Sb = Sb + (cMean-pcaMean)*(cMean-pcaMean)';
+ end
+
+ Sb = n*Sb;
+
+ for i = 1:k
+     cMean = mean(Wpca(:,n*i-(n-1):n*i),2);
+     for j = n*i-(n-1):n*i
+          Sw = Sw + (Wpca(:,j)-cMean)*(Wpca(:,j)-cMean)';
+     end
+ end
